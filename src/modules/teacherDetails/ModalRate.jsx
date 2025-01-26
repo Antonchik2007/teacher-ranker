bb34a8d5ec63090ba76aa4d6cd575fbef56ac563
Anton <antonkurbatov131@gmail.com>
2025-01-26 16:28:15 -0600

    more design updates, made it more strict

--- src/modules/teacherDetails/ModalRate.jsx
+++ src/modules/teacherDetails/ModalRate.jsx
@@ -7,6 +7,7 @@ const ModalRate = ({customButton}) => {
     const [show, setShow] = useState(false);
     const [selectedDifficultyIndex, setSelectedDifficultyIndex] = useState(null)
     const [selectedPhoneIndex, setSelectedPhoneIndex] = useState(null)
+    const [selectedRatingIndex, setSelectedRatingIndex] = useState(null)
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const difficultyRating = [1, 2, 3, 4, 5]
@@ -22,6 +23,9 @@ const ModalRate = ({customButton}) => {
       case 'phone':
         setSelectedPhoneIndex(index)
         break
+      case 'rating':
+        setSelectedRatingIndex(index)
+        break
       }
     }
     return(
@@ -30,18 +34,18 @@ const ModalRate = ({customButton}) => {
 
         <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Rank</Modal.Title>
+          <Modal.Title>Rate</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="modal-teacher-header">
             <p className="modal-teacher-title">{currentTeacher.name}</p>
             <p className="modal-teacher-class">Department: {currentTeacher.schoolDepartment}</p>
+            <p className="modal-teacher-rating">Current rating: {currentTeacher.rating}</p>
           </div>
-            <p className="modal-teacher-rating">Rating: {currentTeacher.rating}</p>
             <p className="modal-teacher-phone">Cell phone usage:</p>
             <div className="modal-teacher-phone-choice">
               {phoneUsage.map((option, index) => {
-                return <p key={index} onClick={() => handleClickSelect(index, 'phone')} className={selectedPhoneIndex === index ? `modal-phone-${option} modal-phone-active`: `modal-phone-${option}`}>{option}</p>
+                return <p key={index} onClick={() => handleClickSelect(index, 'phone')} className={selectedPhoneIndex === index ? `modal-phone modal-phone-${option} modal-phone-active`: `modal-phone modal-phone-${option}`}>{option}</p>
               })}
             </div>
           <p className="modal-class-difficulty">Class difficulty: </p>
@@ -50,6 +54,13 @@ const ModalRate = ({customButton}) => {
               return <p key={index} onClick={() => handleClickSelect(index, 'difficulty')} className={selectedDifficultyIndex === index? 'class-difficulty-rating difficulty-rating-active' : "class-difficulty-rating"}>{rating}</p>
             })}
           </div>
+          <p className="modal-selected-teacher-rating">Overall rating:</p>
+          <div className="modal-class-difficulty-ratings">
+            {difficultyRating.map((rating, index) => {
+              return <p key={index} onClick={() => handleClickSelect(index, 'rating')} className={selectedRatingIndex === index? 'class-difficulty-rating difficulty-rating-active' : "class-difficulty-rating"}>{rating}</p>
+            })}
+          </div>
+          <textarea className="modal-comment" placeholder="Type your comment"></textarea>
         </Modal.Body>
         <Modal.Footer>
           <button className="modal-button-footer">Submit</button>
--- src/modules/teacherDetails/TeacherDetails.jsx
+++ src/modules/teacherDetails/TeacherDetails.jsx
@@ -14,17 +14,17 @@ const TeacherDetails = () => {
             </div>
             <div className="phone-info">
                 <p className="report-text-title">Phone allowance - student reports:</p>
-                <p className="report-text allowed">10 students reported: Allowed</p>
-                <p className="report-text somewhat-allowed">7 Not allowed, but can kinda be used</p>
-                <p className="report-text not-allowed">5 Not allowed, very hard to use</p>
+                <p className="report-text allowed">10 reported: Allowed</p>
+                <p className="report-text somewhat-allowed">7 reported: Can kinda be used</p>
+                <p className="report-text not-allowed">5 reported: Not allowed, very hard to use</p>
             </div>
             <div className="difficulty-info">
                 <p className="difficulty-text difficulty-title">Class difficulty - student reports:</p>
-                <p className="difficulty-text very-easy">10 students reported: Very easy</p>
-                <p className="difficulty-text easy">7 students reported: Easy</p>
-                <p className="difficulty-text medium">7 students reported: Medium</p>
-                <p className="difficulty-text hard">5 students reported: Hard</p>
-                <p className="difficulty-text very-hard">5 students reported: Very hard</p>
+                <p className="difficulty-text very-easy">10 reported: Very easy</p>
+                <p className="difficulty-text easy">7 reported: Easy</p>
+                <p className="difficulty-text medium">7 reported: Medium</p>
+                <p className="difficulty-text hard">5 reported: Hard</p>
+                <p className="difficulty-text very-hard">5 reported: Very hard</p>
             </div>
             <div className="asses-buttons">
                 <ModalRate/>
