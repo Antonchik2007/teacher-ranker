43dd06bc5bebf41c0a943e771ff03126623c0484
Anton <antonkurbatov131@gmail.com>
2025-02-13 17:07:30 -0600

    added visuals when logging in + modified teacher details header

--- src/modules/authentification/LogIn.jsx
+++ src/modules/authentification/LogIn.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import './LogIn.css'
 import { auth } from "../../firebase/firebase";
 import {useAppContext} from '../../AppContext'
@@ -11,17 +11,35 @@ const LogIn = () => {
     const [password, setPassword] = useState('')
     const [data, setData] = useState(null)
     const [error, setError] = useState('')
+    const emailInput = useRef(null)
+    const passwordInput = useRef(null)
+    const handlePasswordChange = (e) => {
+        if(e.target.value.length < 6 && e.target.value.length > 0){
+            passwordInput.current.style.border = '1px solid red'
+        }else{
+            passwordInput.current.style.border = '';
+        }
+        setPassword(e.target.value)
+    }
+    const handleEmailChange = (e) => {
+        if(e.target.value.includes('@') && e.target.value.includes('.com') || e.target.value.length === 0){
+            emailInput.current.style.border = '';
+        }else{
+            emailInput.current.style.border = '1px solid red'
+        }
+        setEmail(e.target.value)
+    }
     return(
         <div className="log-in-wrapper">
             <h1 className="log-in-text" onClick={() => console.log(currentUser.email)}>{isSignedUp ? 'Log in' : 'Sign up'}</h1>
             <div className="form">
                 <div className="email">
                     <p>Email</p>
-                    <input type='email' placeholder="Enter your email" className="log-in-input" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type='email' ref={emailInput} placeholder="Enter your email" className="log-in-input" value={email} onChange={(e) => handleEmailChange(e)}/>
                 </div>
                 <div className="password">
                     <p>Password</p>
-                    <input type='password' placeholder="Enter your password" className="log-in-input" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input type='password' ref={passwordInput} placeholder="Enter your password" className="log-in-input" minLength="6" value={password} onChange={(e) => handlePasswordChange(e)}/>
                 </div>
                 <button className="sign-in-button" onClick={isSignedUp ? (e) => handleLogIn(e, setEmail, setPassword, email, password, setError, setIsLoggedIn, setCurrentUser) : (e) => handleSignUp(e, setEmail, setPassword, email, password, setError, setIsSignedUp)}>{isSignedUp ? 'Log in' : 'Sign up'}</button>
                 <p className="switch-option-text">{isSignedUp ? <p>Don't have an account? <span onClick={() => setIsSignedUp(false)}>Sign up</span></p> : <p>Already have an account? <span onClick={() => setIsSignedUp(true)}>Log in</span></p>}</p>
